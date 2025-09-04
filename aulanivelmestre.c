bffaae500b74cba7e86c61932423fd4998aab4df
diegaoprogramacao <diegaum2278@gmail.com>
2025-09-04 17:39:28 +0000

    aula 2 nivel mestre 1

---
+++ aulanivelmestre.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+
+    int numerosinal = 3000000000;
+    unsigned int numerosemsinal = 300000000;
+
+    printf("Numero com sinal: %d\n", numerosinal);
+    printf("Numero sem sinal: %u\n", numerosemsinal); 
+
+    return 0;
+
+}
