0fb4b77b26ca06443c058dda8da598f81eacd23c
diegaoprogramacao <diegaum2278@gmail.com>
2025-09-05 18:33:45 +0000

    exercicio if idade

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main (){
+
+int idade = 10;
+
+if (idade >= 18){
+    printf("Voce é maior de idade\n");
+    printf("Dentro IF\n"); 
+
+}
+
+    printf("Fora IF\n");
+
+
+}
