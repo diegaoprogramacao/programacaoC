f305e2ab703ce14ac915c7515c53039cfdd765bc
diegaoprogramacao <diegaum2278@gmail.com>
2025-09-05 18:51:31 +0000

    aula3nivelbasico2,3 IF comparação idade

---
+++ .vscode/aula3nivelbasico2,2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+
+int nota; 
+
+nota = 65;
+
+if (nota >= 60){
+    printf("Parabens, voce passou\n");
+    printf("Dentro if\n");
+}
+printf("Fora IF\n");
+
+}
---
+++ .vscode/aula3nivelbasico2,3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+
+    int idade1 = 25;
+    int idade2 = 30;
+
+    if(idade1 > idade2){
+        printf("Pessoa 1 é mais velha que Pessoa 2\n");
+        printf("Dentro IF\n");
+
+    }
+        printf("Fora IF\n");
+}
