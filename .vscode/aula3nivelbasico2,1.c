7de99ee6cacd9acc7653389d85ae87cfd832b3a7
diegaoprogramacao <diegaum2278@gmail.com>
2025-09-05 18:38:37 +0000

    aula 3 nivel basico 2 temperatura

---
+++ .vscode/aula3nivelbasico2,1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+
+float temperatura;
+
+temperatura = 6;
+
+if (temperatura >= 30){
+    printf("Esta calor\n");
+    printf("Dentro IF\n");
+
+}
+printf("Fora IF\n");
+
+}
